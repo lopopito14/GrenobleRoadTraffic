9c22fda34180c95f7ea6eb8dfd78243eee09aa65
Tarik Benchekroun <tarik.benchekroun@mythalesgroup.io>
2020-11-25 21:20:10 +0100

    Bug fixing

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -4,7 +4,7 @@
 
 import 'react-native';
 import React from 'react';
-import Main from '../App/main';
+import Main from '../App/Main';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
